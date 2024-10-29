c97e07aa05652a15682c002a5e664c281aa13c65
AllenKeelig <allen.keeling@atlasschool.com>
2024-10-28 21:39:33 -0500

    10-lint

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,5 +1,5 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const newArray = [];
+  const Array = [];
   let idx = 0;
   for (const value of array) {
     array[idx] = appendString + value;
