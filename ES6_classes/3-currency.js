148c24f1e070c12873e83991a3e90e1a5378186d
AllenKeelig <allen.keeling@atlasschool.com>
2024-10-29 08:14:03 -0500

    3-lint

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -6,24 +6,29 @@ export default class Currency {
     this._code = code;
     this._name = name;
   }
+  
   get name() {
     return this._name;
   }
-  get code() {
-    return this._code;
-  }
+
   set name(newName) {
     if (typeof newName !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = newName;
   }
+
   set code(newCode) {
     if (typeof newCode !== 'string') {
       throw new TypeError('Code must be a string');
     }
     this._code = newCode;
   }
+
+  get code() {
+    return this._code;
+  }
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
