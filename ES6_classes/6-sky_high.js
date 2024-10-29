7917f5bdf6a868154900fc7ba1b2ead91efadf70
AllenKeelig <allen.keeling@atlasschool.com>
2024-10-29 16:41:33 -0500

    6-except im not dumb

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,14 +1,16 @@
 import Building from './5-building.js';
 
 export default class SkyHighBuilding extends Building {
-  constructor(sqft) {
-    if (typeof sqft !== 'number') {
+  constructor(sqft, floors) {
+    super(sqft);
+    if (typeof floors !== 'number') {
       throw new TypeError('Floors must be a number');
     }
+    this._floors = floors;
   }
 
-  get sqft() {
-    return this._sqft;
+  get floors() {
+    return this._floors;
   }
 
   evacuationWarningMessage() {
