a535b5451a000456037ce6fb720db3f855ba9d92
AllenKeelig <allen.keeling@atlasschool.com>
2025-01-10 15:42:42 -0600

    1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -6,10 +6,11 @@ coroutine called async_comprehension that takes no arguments.
 
 import asyncio
 import importlib
+from typing import List
 async_generator = getattr(importlib.import_module("0-async_generator"), "async_generator")
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     """Collect 10 random numbers from async_generator using async comprehension"""
     result = [num async for num in async_generator()]
     return result
