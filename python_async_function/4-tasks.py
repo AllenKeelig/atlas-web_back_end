d193fe2346413c77201769aa3118c4c2428a3f38
AllenKeelig <allen.keeling@atlasschool.com>
2025-01-10 14:02:48 -0600

    4

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+ async routine called wait_n that takes in 2 int arguments
+"""
+
+
+from typing import List
+import asyncio
+import importlib
+task_wait_random = getattr(importlib.import_module("3-tasks.py"),
+                      "task_wait_random")
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """takes 2 int arguments n and max_delay and spawns wait_random"""
+    delays = []
+    for _ in range(n):
+        delays.append(asyncio.create_task(task_wait_random(max_delay)))
+
+    completed_delays = []
+    for task in asyncio.as_completed(delays):
+        completed_delays.append(await task)
+
+    return completed_delays
