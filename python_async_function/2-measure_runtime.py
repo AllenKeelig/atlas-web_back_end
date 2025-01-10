e7e8505af797a7a1e5d90e43c1845f7826c5fdf7
AllenKeelig <allen.keeling@atlasschool.com>
2025-01-10 12:40:53 -0600

    2

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+ measure_time function with integers n and max_delay
+"""
+
+
+from time import perf_counter
+import importlib
+wait_random = getattr(importlib.import_module("1-concurrent_coroutines"),
+                      "wait_n")
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """takes integers n and max_delay and mesures total execution time for wait_n"""
+    start_time = perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    total_time = perf_counter() - start_time
+    return total_time / n
