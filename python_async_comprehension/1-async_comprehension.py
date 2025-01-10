2d865c8b991866d7ec425b013fd7e39b5ea727c9
AllenKeelig <allen.keeling@atlasschool.com>
2025-01-10 15:07:40 -0600

    1 annotated

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -9,7 +9,7 @@ import importlib
 async_generator = getattr(importlib.import_module("0-async_generator"), "async_generator")
 
 
-async def async_comprehension() -> list:
+async def async_comprehension() -> list[float]:
     """Collect 10 random numbers from async_generator using async comprehension"""
     result = [num async for num in async_generator()]
     return result
