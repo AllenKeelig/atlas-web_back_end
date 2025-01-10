aab4766435fa047454038aea80be066178acdd87
AllenKeelig <allen.keeling@atlasschool.com>
2025-01-10 15:02:56 -0600

    1

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+coroutine called async_comprehension that takes no arguments.
+"""
+
+
+import asyncio
+import importlib
+async_generator = getattr(importlib.import_module("0-async_generator"), "async_generator")
+
+
+async def async_comprehension() -> list:
+    """Collect 10 random numbers from async_generator using async comprehension"""
+    result = [num async for num in async_generator()]
+    return result
