11ec5a85653b554a502a91640f50bce6fdd04be6
AllenKeelig <allen.keeling@atlasschool.com>
2025-01-10 10:24:03 -0600

    3-pycodestyle, async new

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """type-annotated function to_str that takes a float n"""
 
+
 def to_str(n: float) -> str:
     """takes a float n as argument and returns the string of the float."""
     return str(n)
