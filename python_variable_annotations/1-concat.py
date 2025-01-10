ff532f71deb66792cc18a3679f97cb40327541b9
AllenKeelig <allen.keeling@atlasschool.com>
2025-01-09 19:43:47 -0600

    1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" type-annotated function add that takes a float a and a float b as arguments"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """takes a float a and a float b as arguments and returns their sum as a float."""
+    return str1 + str2
