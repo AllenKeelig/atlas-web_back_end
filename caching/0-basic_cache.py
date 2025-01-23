9d0e7d870eb804ccc232dcea84284519bfb6db7f
AllenKeelig <allen.keeling@atlasschool.com>
2025-01-22 22:21:09 -0600

    0- documentation

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -4,7 +4,13 @@ from base_caching import BaseCaching
 class BasicCache(BaseCaching):
     """inherits from BaseCaching and is a caching system"""
     def put(self, key, item):
+        """
+        assign to the dictionary self.cache_data the item value for the key
+        """
         if key is not None and item is not None:
             self.cache_data[key] = item
     def get(self, key):
+        """
+        return the value in self.cache_data linked to key.
+        """
         return self.cache_data.get(key)
