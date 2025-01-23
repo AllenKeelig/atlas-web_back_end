09d63833d5ab2484f1a67db169b5780e1f4cfe84
AllenKeelig <allen.keeling@atlasschool.com>
2025-01-22 22:22:34 -0600

    0-pep8

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """class BasicCache that inherits from BaseCaching"""
 from base_caching import BaseCaching
+
+
 class BasicCache(BaseCaching):
     """inherits from BaseCaching and is a caching system"""
     def put(self, key, item):
         """
         assign to the dictionary self.cache_data the item value for the key
         """
+
         if key is not None and item is not None:
             self.cache_data[key] = item
     def get(self, key):
