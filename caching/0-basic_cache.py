f2ce9b36b4807ae993eb3763b6495aa2aa9b8a00
AllenKeelig <allen.keeling@atlasschool.com>
2025-01-22 21:43:17 -0600

    0

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""class BasicCache that inherits from BaseCaching"""
 from base_caching import BaseCaching
 class BasicCache(BaseCaching):
+    """inherits from BaseCaching and is a caching system"""
     def put(self, key, item):
         if key is not None and item is not None:
             self.cache_data[key] = item
