deb410edc3e6fb5e91ecd1eaac4ff0abe25ac972
AllenKeelig <allen.keeling@atlasschool.com>
2025-01-24 01:02:06 -0600

    readme

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -5,7 +5,6 @@
 def index_range(page, page_size):
     """
     takes two integer arguments page and page_size
-    The function should return a tuple of size two
     """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
