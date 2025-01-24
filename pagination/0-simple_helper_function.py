713f4177f65cffc0bece24eedfacc0eb612061e0
AllenKeelig <allen.keeling@atlasschool.com>
2025-01-24 01:00:13 -0600

    0

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Simple helper function"""
+
+
+def index_range(page, page_size):
+    """
+    takes two integer arguments page and page_size
+    The function should return a tuple of size two
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
