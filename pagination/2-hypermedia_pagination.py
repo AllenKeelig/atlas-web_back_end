746c0f735452dc8ed9a293e523bc0f5be2b1e886
AllenKeelig <allen.keeling@atlasschool.com>
2025-01-24 23:29:03 -0600

    2

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -34,6 +34,22 @@ class Server:
         start_index, end_index = index_range(page, page_size)
         return dataset[start_index:end_index] \
             if start_index < len(dataset) else []
+    
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """returns a dictionary"""
+        data = self.get_page(page, page_size)
+        items = len(self.dataset())
+        total_pages = math.ceil(items / page_size)
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages,
+        }
 
 
 def index_range(page, page_size):
@@ -43,20 +59,3 @@ def index_range(page, page_size):
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
-
-
-def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
-    """returns a dictionary"""
-    data = self.get_page(page, page_size)
-    items = len(self.dataset())
-    total_pages = math.ceil(items / page_size)
-    next_page = page + 1 if page < total_pages else None
-    prev_page = page - 1 if page > 1 else None
-    return {
-            "page_size": len(data),
-            "page": page,
-            "data": data,
-            "next_page": next_page,
-            "prev_page": prev_page,
-            "total_pages": total_pages,
-        }
