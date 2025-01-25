9637bc7a7d9460e7d205378de89c2073ed2dbc83
AllenKeelig <allen.keeling@atlasschool.com>
2025-01-25 00:17:34 -0600

    0 pycodestyle

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" returns the log message obfuscated """
+""" Regex-ing """
 import re
 from typing import List
 
@@ -10,5 +10,9 @@ def filter_datum(
         message: str,
         separator: str
 ) -> str:
+    """returns the log message obfuscated"""
     pattern = f"({'|'.join((field) for field in fields)})=.+?{separator}"
-    return re.sub(pattern, lambda match: f"{match.group(1)}={redaction}{separator}", message)
+    return re.sub(
+        pattern,
+        lambda match: f"{match.group(1)}={redaction}{separator}",
+        message)
