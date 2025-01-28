24db7d890cb26fe2f1d4601c3749d395afc29c13
AllenKeelig <allen.keeling@atlasschool.com>
2025-01-28 09:00:45 -0600

    1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
-""" Regex-ing """
+""" Log formatter """
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        unencrypted = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, unencrypted, self.SEPARATOR)
 
 
 def filter_datum(
