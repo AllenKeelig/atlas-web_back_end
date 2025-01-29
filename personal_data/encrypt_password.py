fd746d23041a92c83adc88f10134a36951988c57
AllenKeelig <allen.keeling@atlasschool.com>
2025-01-29 13:56:00 -0600

    6

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -8,3 +8,7 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode(), salt)
     return hashed_password
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if the provided password matches the stored hashed password."""
+    return bcrypt.checkpw(password.encode(), hashed_password)
