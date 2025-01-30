1b882e11546c6381bf0f30aabe81378c9c7bc85f
AllenKeelig <allen.keeling@atlasschool.com>
2025-01-29 18:15:14 -0600

    5

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -14,8 +14,12 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth_type = getenv("AUTH_TYPE")
-if auth_type == "Auth":
+if os.getenv('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
     auth = Auth()
+elif os.getenv('AUTH_TYPE') == "basic_auth":
+    from api.v1.auth.basic_auth import basic_auth
+    auth = BasicAuth()
 
 
 def before_request():
