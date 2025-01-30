6dcc6d32a4ccc1cca5b4ea3693371e225a3f4e55
AllenKeelig <allen.keeling@atlasschool.com>
2025-01-29 18:09:27 -0600

    5

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -12,6 +12,24 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth_type = getenv("AUTH_TYPE")
+if auth_type == "Auth":
+    auth = Auth()
+
+
+def before_request():
+    """Filter requests to secure the API."""
+    if auth is None:
+        return
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if request.path in excluded_paths:
+        return
+    if auth.require_auth(request.path, excluded_paths):
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 @app.errorhandler(404)
