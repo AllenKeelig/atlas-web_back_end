ba02782b8df9b457b92ad541bc15e1eb19c988e3
AllenKeelig <allen.keeling@atlasschool.com>
2025-01-29 18:34:12 -0600

    6

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Basic Authentication class for the API
+"""
+
+from api.v1.auth.auth import Auth
+
+class BasicAuth(Auth):
+    """BasicAuth class inherits from Auth."""
+    pass
