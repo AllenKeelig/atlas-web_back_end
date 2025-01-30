8ff136dfe0fc12fa57ee006cc7a43358f44ee8e6
AllenKeelig <allen.keeling@atlasschool.com>
2025-01-29 21:06:07 -0600

    10

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,9 @@ Basic Authentication class for the API
 from api.v1.auth.auth import Auth
 import base64
 from typing import Tuple
+from typing import TypeVar
+from models.user import User
+
 
 
 class BasicAuth(Auth):
