917387eea742e6cc7a2132b981c82b9bf60d25b7
AllenKeelig <allen.keeling@atlasschool.com>
2025-01-29 20:22:50 -0600

    6

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -9,18 +9,6 @@ from flask_cors import (CORS, cross_origin)
 import os
 
 
-app = Flask(__name__)
-app.register_blueprint(app_views)
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = None
-auth_type = getenv("AUTH_TYPE")
-if os.getenv('AUTH_TYPE') == 'auth':
-    from api.v1.auth.auth import Auth
-    auth = Auth()
-elif os.getenv('AUTH_TYPE') == "basic_auth":
-    from api.v1.auth.basic_auth import basic_auth
-    auth = BasicAuth()
-
 @app.before_request
 def before_request():
     """Filter requests to secure the API."""
@@ -38,24 +26,38 @@ def before_request():
             abort(403)
 
 
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Not found"}), 404
+class BasicAuth(Auth):
+    """Auth class managing authentication"""
+    app = Flask(__name__)
+    app.register_blueprint(app_views)
+    CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+    auth = None
+    auth_type = getenv("AUTH_TYPE")
+    if os.getenv('AUTH_TYPE') == 'auth':
+        from api.v1.auth.auth import Auth
+        auth = Auth()
+    elif os.getenv('AUTH_TYPE') == "basic_auth":
+        from api.v1.auth.basic_auth import basic_auth
+        auth = BasicAuth()
+
+    @app.errorhandler(404)
+    def not_found(error) -> str:
+        """ Not found handler
+        """
+        return jsonify({"error": "Not found"}), 404
 
 
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """Unauthorized handler"""
-    return jsonify({"error": "Unauthorized"}), 401
+    @app.errorhandler(401)
+    def unauthorized(error) -> str:
+        """Unauthorized handler"""
+        return jsonify({"error": "Unauthorized"}), 401
 
 
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """ Forbidden handler
-    """
-    return jsonify({"error": "Forbidden"}), 403
+    @app.errorhandler(403)
+    def forbidden(error) -> str:
+        """ Forbidden handler
+        """
+        return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
