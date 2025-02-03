d1464733b7e9cec4901f67e032fbb719a4780acb
AllenKeelig <allen.keeling@atlasschool.com>
2025-02-03 15:54:47 -0600

    2

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -27,20 +27,26 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(context.exception.args[0], expected)
 
 class TestGetJson(unittest.TestCase):
+    
     @patch("utils.requests.get")
     def test_get_json(self, mock_get):
         test_cases = [
             ("http://example.com", {"payload": True}),
             ("http://holberton.io", {"payload": False})
         ]
+        
         for test_url, test_payload in test_cases:
             mock_response = Mock()
             mock_response.json.return_value = test_payload
             mock_get.return_value = mock_response
+            
             result = utils.get_json(test_url)
+            
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+            
             mock_get.reset_mock()
 
 if __name__ == "__main__":
     unittest.main()
+
