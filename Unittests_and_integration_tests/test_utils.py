ce3fe41869e3aec83d5b4c3c562785a2a3baed17
AllenKeelig <allen.keeling@atlasschool.com>
2025-02-03 15:05:54 -0600

    1

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -24,5 +24,24 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
         self.assertEqual(context.exception.args[0], expected)
 
+class TestGetJson(unittest.TestCase):
+    """ test that it returns expected results """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """ test get_json """
+        # create mock response
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        result = get_json(test_url)
+        # assertions
+        self.assertEqual(result, test_payload)
+        mock_get.assert_called_once_with(test_url)
+
 if __name__ == "__main__":
     unittest.main()
