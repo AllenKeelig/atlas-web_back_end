e0e348f6ba3258498396673557cb56f7ad303685
AllenKeelig <allen.keeling@atlasschool.com>
2025-02-03 15:53:07 -0600

    2

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -27,23 +27,20 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(context.exception.args[0], expected)
 
 class TestGetJson(unittest.TestCase):
-    """ test that it returns expected results """
-    @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False}),
-    ])
-    @patch('requests.get')
-    def test_get_json(self, test_url, test_payload, mock_get):
-        """ test get_json """
-        # create mock response
-        mock_response = Mock()
-        mock_response.json.return_value = test_payload
-        mock_get.return_value = mock_response
-
-        result = get_json(test_url)
-        # assertions
-        self.assertEqual(result, test_payload)
-        mock_get.assert_called_once_with(test_url)
+    @patch("utils.requests.get")
+    def test_get_json(self, mock_get):
+        test_cases = [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False})
+        ]
+        for test_url, test_payload in test_cases:
+            mock_response = Mock()
+            mock_response.json.return_value = test_payload
+            mock_get.return_value = mock_response
+            result = utils.get_json(test_url)
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
+            mock_get.reset_mock()
 
 if __name__ == "__main__":
     unittest.main()
