151f4daffb3090019a5107dd72a279e6df4dd3f1
AllenKeelig <allen.keeling@atlasschool.com>
2025-02-04 18:14:33 -0600

    7

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -65,6 +65,20 @@ class TestGithubOrgClient(unittest.TestCase):
                 "https://api.github.com/orgs/test_org/repos"
                 )
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test GithubOrgClient.has_license"""
+        self.assertEqual(
+            GithubOrgClient.has_license(
+                repo,
+                license_key
+                ),
+                expected
+            )
+
 
 if __name__ == "__main__":
     unittest.main()
