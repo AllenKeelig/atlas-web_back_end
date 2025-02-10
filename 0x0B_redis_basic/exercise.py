2830d4242ee285509cca23d8ebc096bcbeb07270
AllenKeelig <allen.keeling@atlasschool.com>
2025-02-10 07:53:53 -0600

    3

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -13,6 +13,19 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+def call_history(method: Callable) -> Callable:
+    """Decorator to store the history of inputs and outputs for a function."""
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        inputs_key = method.__qualname__ + ":inputs"
+        outputs_key = method.__qualname__ + ":outputs"
+        
+        self._redis.rpush(inputs_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(outputs_key, str(output))
+        return output
+    return wrapper
+
 class Cache:
     def __init__(self):
         """Initialize the Redis client and flush the database."""
@@ -20,6 +33,7 @@ class Cache:
         self._redis.flushdb()
     
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in Redis with a randomly generated key."""
         key = str(uuid.uuid4())
