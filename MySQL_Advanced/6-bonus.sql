6846e6197fb46485e7f2e0854f23175413f2b2b8
AllenKeelig <allen.keeling@atlasschool.com>
2025-02-11 12:34:39 -0600

    6

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -5,10 +5,9 @@ BEGIN
     DECLARE project_id INT;
     SELECT id INTO project_id FROM projects WHERE name = project_name;
     IF project_id IS NULL THEN
-      INSERT INTO projects (name) VALUES (project_name);
-      SET project_id = LAST_INSERT_ID();
+        INSERT INTO projects (name) VALUES (project_name);
+        SET project_id = LAST_INSERT_ID();
     END IF;
-    UPDATE students
     INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
 END //
 DELIMITER ;
