1745e9b3a487c4b7d6a1a2ee39e14f77b64c82d6
AllenKeelig <allen.keeling@atlasschool.com>
2025-02-11 09:53:18 -0600

    1

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,8 @@
 -- creates table
 -- does not do anything if table exists
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
