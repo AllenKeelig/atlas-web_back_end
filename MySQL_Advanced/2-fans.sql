cba4a40c847ba1c3174bf6e6fc024f2383724ecf
AllenKeelig <allen.keeling@atlasschool.com>
2025-02-11 10:16:15 -0600

    2

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,6 +1,7 @@
---ranks country origins of bands
---completely objectively
-SELECT origin, SUM(fans) AS nb_fans
+-- ranks countries by bands
+SELECT 
+    origin,
+    SUM(fans) as nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
