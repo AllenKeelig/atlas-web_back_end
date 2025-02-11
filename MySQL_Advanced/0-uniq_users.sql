b5e8deaf4d3670a0650d39f2a39501a57631133b
AllenKeelig <allen.keeling@atlasschool.com>
2025-02-11 09:49:55 -0600

    0

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,5 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-SELECT id, name FROM students WHERE batch_id = 3 ORDER BY created_at DESC LIMIT 3;
-
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
