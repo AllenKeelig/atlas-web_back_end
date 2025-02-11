11e37d785fcdf229d1fc45e4ed4068809f0af7a7
AllenKeelig <allen.keeling@atlasschool.com>
2025-02-11 12:28:41 -0600

    6

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -3,3 +3,17 @@ DELIMITER //
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
     DECLARE project_id INT;
+    SELECT id INTO project_id
+    FROM projects
+    WHERE name = project_name;
+    INSERT INTO bonuses (user_id, project_id, score)
+    VALUES (user_id, project_id, score);
+    UPDATE students
+    SET average_score = (
+        SELECT AVG(score)
+        FROM bonuses
+        WHERE user_id = user_id
+    )
+    WHERE id = user_id;
+END //
+DELIMITER ;
