c1aa94ace1320c153947f6c4c1f702a5d27fc54a
AllenKeelig <allen.keeling@atlasschool.com>
2025-02-11 12:44:18 -0600

    8

--- MySQL_Advanced/8-index_my_names.sql
+++ MySQL_Advanced/8-index_my_names.sql
@@ -1,4 +1,2 @@
 -- creates an index idx_name_first on the table names and the first letter of name.
-ALTER TABLE names
-ADD COLUMN first_letter CHAR(1) AS (LEFT(name, 1)) STORED;
-CREATE INDEX idx_name_first ON names(first_letter);
+CREATE INDEX idx_name_first ON names(name(1));
