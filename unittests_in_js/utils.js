0301b1c3b710acaeb6efdfb7e9caeffea4a684e4
AllenKeelig <allen.keeling@atlasschool.com>
2025-03-05 21:03:00 -0600

    3

--- unittests_in_js/utils.js
+++ unittests_in_js/utils.js
@@ -1,18 +1,17 @@
-// utils.js
 const Utils = {
-  calculateNumber: function (type, a, b) {
-    if (type === 'SUM') {
-      return Math.round(a) + Math.round(b);
+    calculateNumber: function (type, a, b) {
+      if (type === 'SUM') {
+        return Math.round(a) + Math.round(b);
+      }
+      if (type === 'SUBTRACT') {
+        return Math.round(a) - Math.round(b);
+      }
+      if (type === 'DIVIDE') {
+        if (b === 0) return 'Error';
+        else return Math.round(a) / Math.round(b);
+      }
+      return 'Invalid Operation';
     }
-    if (type === 'SUBTRACT') {
-      return Math.round(a) - Math.round(b);
-    }
-    if (type === 'DIVIDE') {
-      if (b === 0) return 'Error';  // Ensure this is a string
-      return Math.round(a) / Math.round(b);
-    }
-    return 'Invalid Operation';
-  }
-};
-
-module.exports = Utils;
+  };
+  
+  module.exports = Utils;
