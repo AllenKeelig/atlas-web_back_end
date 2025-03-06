07be31080a7b55732f61381c7cd07f0db8143ab8
AllenKeelig <allen.keeling@atlasschool.com>
2025-03-05 21:13:17 -0600

    3

--- unittests_in_js/4-payment.test.js
+++ unittests_in_js/4-payment.test.js
@@ -1,7 +1,9 @@
+
 const sinon = require('sinon');
 const assert = require('chai').assert;
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
+
 describe('sendPaymentRequestToApi', function () {
   it('should call Utils.calculateNumber with correct arguments', function () {
     const spy = sinon.spy(Utils, 'calculateNumber');
