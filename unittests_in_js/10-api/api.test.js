2aa045b7a51c59fe012a0d276194569763419031
AllenKeelig <allen.keeling@atlasschool.com>
2025-03-05 22:57:38 -0600

    10

--- unittests_in_js/10-api/api.test.js
+++ unittests_in_js/10-api/api.test.js
@@ -13,10 +13,8 @@ describe('Index page', () => {
   it('Cart endpoint handles integer IDs', (done) => {
     request.get('http://localhost:7865/cart/12', (error, response, body) => {
       if (error) return done(error);
-
       expect(response.statusCode).to.equal(200);
       const expectedResponse = 'Payment methods for cart 12';
-
       expect(body).to.equal(expectedResponse);
       done();
     });
@@ -24,8 +22,40 @@ describe('Index page', () => {
   it('Cart endpoint rejects non-integer IDs', (done) => {
     request.get('http://localhost:7865/cart/hello', (error, response, body) => {
       if (error) return done(error);
-
       expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
+describe('Payments page', () => {
+  it('Payments endpoint returns correct structure', (done) => {
+    request.get('http://localhost:7865/available_payments', (error, response, body) => {
+      if (error) return done(error);
+      expect(response.statusCode).to.equal(200);
+      const parsedBody = JSON.parse(body);
+      const expectedResponse = {
+        payment_methods: {
+          credit_cards: true,
+          paypal: false
+        }
+      };
+      expect(parsedBody).to.deep.equal(expectedResponse);
+      done();
+    });
+  });
+});
+describe('Login page', () => {
+  it('Login endpoint displays welcome message with username', (done) => {
+    const userName = 'Betty';
+    request.post({
+      url: 'http://localhost:7865/login',
+      json: true,
+      body: { "userName": userName }
+    }, (error, response, body) => {
+      if (error) return done(error);
+      expect(response.statusCode).to.equal(200);
+      const expectedResponse = `Welcome ${userName}`;
+      expect(body).to.deep.equal(expectedResponse);
       done();
     });
   });
