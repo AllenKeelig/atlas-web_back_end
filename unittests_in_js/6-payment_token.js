b2edd84cae4560b7be05d193864642548c650a0e
AllenKeelig <allen.keeling@atlasschool.com>
2025-03-05 21:58:12 -0600

    6

--- unittests_in_js/6-payment_token.js
+++ unittests_in_js/6-payment_token.js
@@ -1,9 +1,12 @@
 // 6-payment_token.js
 function getPaymentTokenFromAPI(success) {
   if (success) {
-    return Promise.resolve({ data: 'Successful response from the API' });
+    return Promise.resolve({
+      data: 'Successful response from the API'
+    });
   }
-}
+
+  return;
 }
 
 module.exports = getPaymentTokenFromAPI;
